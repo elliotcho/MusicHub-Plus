6860d1437f66d0eb28eabbd78888e4c935b5b83a
Yong <ycho97@uwo.ca>
2021-01-26 13:41:40 -0500

    update cache after like

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,8 +3,10 @@ import { Box, IconButton, Stack } from '@chakra-ui/react';
 import { ChevronDownIcon, ChevronUpIcon, DeleteIcon } from '@chakra-ui/icons';
 import Navbar from '../components/Navbar';
 import { withApollo } from '../utils/withApollo';
-import { useDeleteSongMutation, useDislikeSongMutation, useLikeSongMutation, useMeQuery, useSongsQuery } from '../generated/graphql';
+import { LikeSongMutation, useDeleteSongMutation, useDislikeSongMutation, useLikeSongMutation, useMeQuery, useSongsQuery } from '../generated/graphql';
 import { isServer } from '../utils/isServer';
+import gql from 'graphql-tag';
+import { ApolloCache } from '@apollo/client';
 
 const Index: React.FC<{}> = ({}) => {
   const { data, loading } = useSongsQuery();
@@ -17,15 +19,63 @@ const Index: React.FC<{}> = ({}) => {
   const [dislikeSong] = useDislikeSongMutation();
   const [likeSong] = useLikeSongMutation();
 
-  document.addEventListener('play', e => {
-    const audioList = document.getElementsByTagName('audio');
+  if(!isServer()){
+    document.addEventListener('play', e => {
+      const audioList = document.getElementsByTagName('audio');
+  
+      for(let i=0;i<audioList.length;i++){
+         if(audioList[i] !== e.target) {
+            audioList[i].pause(); 
+         }
+      }
+    }, true);
+  }
 
-    for(let i=0;i<audioList.length;i++){
-       if(audioList[i] !== e.target) {
-          audioList[i].pause(); 
-       }
+  const updateAfterLike = (
+     cache: ApolloCache<LikeSongMutation >,
+     songId: number
+  ) => {
+    const data = cache.readFragment<{
+      id: number
+      ratingStatus: number | null;
+      likes: number;
+    }>({
+      id: 'Song:' + songId,
+      fragment: gql`
+         fragment _ on Song {
+            id
+            ratingStatus
+            likes
+         }
+      `
+    });
+
+    if(data) {
+      const { ratingStatus, likes } = data;
+       
+      let newValue = 0;
+      let newStatus = 0;
+      
+      if(ratingStatus === 1) {
+         newValue = likes - 1;
+         newStatus = 0;
+      } else {
+         newValue = likes + 1;
+         newStatus = 1;
+      }
+
+       cache.writeFragment({
+          id: 'Song:' + songId,
+          fragment: gql`
+            fragment _ on Song {
+              ratingStatus
+              likes
+            }
+          `,
+          data: { likes: newValue, ratingStatus: newStatus }
+       });
     }
-  }, true);
+  }
 
   return (
     <>
@@ -36,7 +86,7 @@ const Index: React.FC<{}> = ({}) => {
         {!loading && data.songs.map( ({
            id, title, url, ratingStatus, likes, dislikes, user: { id: userId, username }
         }) => 
-            <Box key={id} my={8}>
+            <Box key={id} my={8} p={8}>
               <Box>
                 {title} posted by {username}
               </Box>
@@ -70,7 +120,10 @@ const Index: React.FC<{}> = ({}) => {
                     colorScheme = {ratingStatus === 1? 'green' : undefined}
                     onClick = {async () => {
                        await likeSong({
-                         variables: { songId: id }
+                         variables: { songId: id },
+                         update: (cache) => {
+                            updateAfterLike(cache, id);
+                         }
                        });
                     }}
                  />
